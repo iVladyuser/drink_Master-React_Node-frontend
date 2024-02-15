b398315c84ba8835785c34511882c8a065e42102
ciplator <ciplator@gmail.com>
2024-02-15 22:18:45 +0200

    logo

---
+++ src/components/Logo/Logo.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import logo from '../../images/logo.svg';
+
+const Logo = () => {
+  return (
+    <div>
+        <Logo />
+        {}
+    </div>
+);
+};
+export default Logo;
