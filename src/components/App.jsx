126fa4acb9cc9dbdc06a6f2972adafce51eecb2c
iVladyuser <korol.v09@gmail.com>
2024-02-17 09:55:08 +0100

    dddd

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,12 +5,18 @@ import { Loader } from './Loader/Loader';
 import { useDispatch } from 'react-redux';
 import { refreshThunk } from 'services/fetchAuth';
 import * as ROUTES from 'constants/routes';
-import RestrictedRoute from './RestrictedRoute';
-// import PrivateRoute from './PrivateRoute';
 
-const Home = lazy(() => import('pages/HomePage'));
-const Register = lazy(() => import('pages/RegisterPage'));
-const Login = lazy(() => import('pages/LogInPage'));
+const Welcome = lazy(() => import('pages/WelcomePages/WelcomePage'));
+const Register = lazy(() => import('pages/WelcomePages/SignUpPage/SignUpPage'));
+const Login = lazy(() => import('pages/WelcomePages/SignInPage/SignInPage'));
+const AddDrinkPage = lazy(() => import('pages/AddDrinkPages/AddDrinkPages'));
+const Drinks = lazy(() => import('pages/DrinksPages/DrinksPages'));
+const FavoriteDrinksPage = lazy(() =>
+  import('pages/FavoritePage/FavoriteDrinksPage')
+);
+
+// import RestrictedRoute from './RestrictedRoute';
+// import PrivateRoute from './PrivateRoute';
 
 const appRoutes = [
   { path: ROUTES.HOME_ROUTE, element: <Home /> },
@@ -30,6 +36,15 @@ const appRoutes = [
       </RestrictedRoute>
     ),
   },
+  {
+    path: ROUTES.SIGNUP_ROUTE,
+    element: <Register />,
+  },
+
+  { path: ROUTES.FAVORITES_ROUTE, element: <FavoriteDrinksPage /> },
+
+  { path: ROUTES.ADDDRINK_ROUTE, element: <AddDrinkPage /> },
+  { path: ROUTES.DRINKS_ROUTE, element: <Drinks /> },
 ];
 
 const App = () => {
