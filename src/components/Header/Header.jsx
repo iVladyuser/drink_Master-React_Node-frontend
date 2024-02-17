16bd274105068374bca9105dba0f9bf0380efc26
ciplator <ciplator@gmail.com>
2024-02-17 14:02:23 +0200

    Navigation

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { HeaderContainer } from './Header.styled';
+import { GiHamburgerMenu } from 'react-icons/gi';
 import Logo from '../Logo/Logo';
+import Navigation from './Navigation/Navigation';
+
 export const Header = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const toggleModal = () => {
+    setIsModalOpen(!isModalOpen);
+  };
+
   return (
     <HeaderContainer>
-      <Logo />
-      <h1>Welcome to the Home Page</h1>
-
+      <a href="./"><Logo /></a>
+      <a href="./">Drink Master</a>
+      <GiHamburgerMenu onClick={toggleModal} />
+      {isModalOpen && <Navigation onClose={toggleModal} />}
     </HeaderContainer>
   );
 };
+
---
+++ src/components/Header/Navigation/Navigation.styled.jsx
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+
+export const NavigationContainer = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const NavigationContent = styled.div`
+  background-color: #fff;
+  padding: 20px;
+  border-radius: 10px;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+`;
