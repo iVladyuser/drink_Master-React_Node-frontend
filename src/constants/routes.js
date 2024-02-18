35b28e53c483be0483f31cc6554007698346f765
VitaPerevoznikova <frayvita@gmail.com>
2024-02-18 12:45:16 +0200

    add app styled,home route

--- src/constants/routes.js
+++ src/constants/routes.js
@@ -1,9 +1,8 @@
 export const WELCOME_ROUTE = '/welcome';
 export const SIGNUP_ROUTE = '/signup';
 export const SIGNIN_ROUTE = '/signin';
+
+export const HOME_ROUTE = '/home';
 export const ADDDRINK_ROUTE = '/adddrink';
-export const HOME_ROUTE = '/';
-
 export const FAVORITES_ROUTE = '/favorites';
-
 export const DRINKS_ROUTE = '/drinks';
