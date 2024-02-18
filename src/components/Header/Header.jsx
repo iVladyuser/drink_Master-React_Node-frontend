a0791834d4fd1e13f23e2db93545f0f18a5eadb2
ciplator <ciplator@gmail.com>
2024-02-18 22:40:57 +0200

    contanier

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -14,6 +14,7 @@ export const Header = () => {
   return (
     <HeaderContainer>
       <Logo />
+      
       <StyledGiHamburgerMenu onClick={toggleModal} />
       <Navigation onClose={toggleModal} isVisible={isModalOpen} />
     </HeaderContainer>
