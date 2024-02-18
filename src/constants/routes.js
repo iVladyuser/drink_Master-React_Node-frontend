0a74a2ef9e80a4f334cdf8897b5260f59baab76c
Mark <markiyan.chornovil@gmail.com>
2024-02-18 19:57:26 +0100

    added header & footer to adddrink

--- src/constants/routes.js
+++ src/constants/routes.js
@@ -3,7 +3,7 @@ export const SIGNUP_ROUTE = '/signup';
 export const SIGNIN_ROUTE = '/signin';
 
 export const HOME_ROUTE = '/home';
-export const ADDDRINK_ROUTE = '/adddrink';
+export const ADDDRINK_ROUTE = '/add';
 export const FAVORITES_ROUTE = '/favorites';
 export const DRINKS_ROUTE = '/drinks';
 export const DRINKPAGE_ROUTE = '/drink/:drinkId';
