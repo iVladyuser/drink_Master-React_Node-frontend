8799817f12876dd1e5695b0f07641d1a61e574bb
KuturlanovaMaryna <129969564+KuturlanovaMaryna@users.noreply.github.com>
2024-02-19 13:36:12 +0200

    signup

--- src/components/SignUpSignInForms/SignUpForm/SignUpForm.jsx
+++ src/components/SignUpSignInForms/SignUpForm/SignUpForm.jsx
@@ -1,26 +1,26 @@
 import { React } from 'react';
 
-import * as Yup from 'yup';
+// import * as Yup from 'yup';
 import { useDispatch } from 'react-redux';
-import { toast } from 'react-toastify';
-import { signUp } from '../../../redux/auth/operations';
+// import { toast } from 'react-toastify';
+import { signUpThunk } from '../../../services/fetchAuth';
 // import StyledDatePicker from '../../DatePicker/DatePicker';
 
 import { AuthForm, Input, Button, SignInLink } from './SignUp.styled';
 
-const validateFormSchema = Yup.object().shape({
-  name: Yup.string()
-    .min(2, 'The name is short.')
-    .required('Field is required.'),
-  dateBirth: Yup.string().required('Field is required.'),
-  email: Yup.string()
-    .email('Please enter a valid email')
-    .required('Field is required.'),
-  password: Yup.string()
-    .min(8, 'Password must contain 8 characters.')
-    .max(20, 'Password is too Long!')
-    .required('Field is required.'),
-});
+// const validateFormSchema = Yup.object().shape({
+//   name: Yup.string()
+//     .min(2, 'The name is short.')
+//     .required('Field is required.'),
+//   dateBirth: Yup.string().required('Field is required.'),
+//   email: Yup.string()
+//     .email('Please enter a valid email')
+//     .required('Field is required.'),
+//   password: Yup.string()
+//     .min(8, 'Password must contain 8 characters.')
+//     .max(20, 'Password is too Long!')
+//     .required('Field is required.'),
+// });
 
 export const SignUpForm = () => {
   const dispatch = useDispatch();
@@ -36,13 +36,10 @@ export const SignUpForm = () => {
     console.log('email:', email);
     console.log('password:', password);
 
-    dispatch(signUp({ name, email, password }))
-      .unwrap()
-      .then(() => toast.success('Registration successfully'))
-      .catch(() => toast.error('Something went wrong. Try again'));
+    dispatch(signUpThunk({ name, email, password }));
   };
   return (
-    <AuthForm validationSchema={validateFormSchema} onSubmit={handleSubmit}>
+    <AuthForm onSubmit={handleSubmit}>
       <>
         <Input type="text" name="userName" placeholder="Name" required />
 
