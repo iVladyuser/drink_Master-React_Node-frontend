7798b1ad680797ebf6f049d439c4f962bcef8763
OksanaLukashyk <130361592+OksanaLukashyk@users.noreply.github.com>
2024-02-20 23:23:18 +0200

    changes

--- src/redux/drink/drink.selectors.js
+++ src/redux/drink/drink.selectors.js
@@ -1,7 +1,7 @@
 export const selectDrinkById = state => state.drink.drink;
-export const selectIngredients = state => state.drink.inredients;
+// export const selectIngredients = state => state.drink.inredients;
 export const selectIsLoading = state => state.drink.isLoading;
 export const selectError = state => state.drink.error;
 
-// Home 
-export const drinksSelector = (state) => state.drinks.drinks;
+// Home
+export const drinksSelector = state => state.drinks.drinks;
