3bad56c35bffc760ac4004d5db939c1ca559a1d1
Dmitry Sukhorukov <ciplator@gmail.com>
2024-02-22 13:56:36 +0200

    Update

--- src/components/Header/LogoutButton/LogoutButton.jsx
+++ src/components/Header/LogoutButton/LogoutButton.jsx
@@ -1,24 +1,44 @@
-import React from 'react';
-
-import { StyledButtonOut } from './LogoutButton.styled';
+import React, { useState } from 'react';
+import { StyledButtonOut, ModalOverlay, ModalContent, ModalButton, ModalCancelButton, ModalWrapper } from './LogoutButton.styled';
 import { logOutThunk } from 'services/fetchAuth';
 import { useDispatch } from 'react-redux';
 import { toast } from 'react-toastify';
 
 const LogoutButton = () => {
   const dispatch = useDispatch();
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
   const handleLogout = () => {
     dispatch(logOutThunk())
       .unwrap()
       .then(() => {
-        toast.success('LogOut successful');
+        toast.success('Logout successful');
       })
       .catch(() => {
         toast.error('Something went wrong... Try again...');
       });
   };
 
-  return <StyledButtonOut onClick={handleLogout}>Logout</StyledButtonOut>;
+  const toggleModal = () => {
+    setIsModalOpen(!isModalOpen);
+  };
+
+  return (
+    <>
+      <StyledButtonOut onClick={toggleModal}>Logout</StyledButtonOut>
+      {isModalOpen && (
+        <ModalOverlay>
+          <ModalWrapper>
+            <ModalContent>
+              <p>Are you sure you want to log out?</p>
+              <ModalButton onClick={handleLogout}>Log out</ModalButton>
+              <ModalCancelButton onClick={toggleModal}>Cancel</ModalCancelButton>
+            </ModalContent>
+          </ModalWrapper>
+        </ModalOverlay>
+      )}
+    </>
+  );
 };
 
 export default LogoutButton;
--- src/components/Header/Navigation/Navigation.jsx
+++ src/components/Header/Navigation/Navigation.jsx
@@ -1,9 +1,8 @@
 import React, { useState, useEffect } from 'react';
-// import { Link } from 'react-router-dom';
-import  Logo  from '../../Logo/Logo';
+import Logo from '../../Logo/Logo';
 import ThemeSwitcher from '../Switch/ThemeSwitcher';
-import NavigationLink from '../Navigation/ButtonLink/ButtonLink'
-import { NavigationContainer, LogoSwitcherStyled, CloseButton, ThemeSwitcherCloseBtn,} from './Navigation.styled';
+import NavigationLink from '../Navigation/ButtonLink/ButtonLink';
+import { NavigationContainer, LogoSwitcherStyled, CloseButton, ThemeSwitcherCloseBtn } from './Navigation.styled';
 
 const Navigation = ({ onClose, isVisible }) => {
     const [animationClass, setAnimationClass] = useState('');
@@ -16,16 +15,19 @@ const Navigation = ({ onClose, isVisible }) => {
         }
     }, [isVisible]);
 
+    const handleCloseModal = () => {
+        onClose();
+    };
+
     return (
         <NavigationContainer className={animationClass}>
-
-<LogoSwitcherStyled>
-          <Logo />
-          <ThemeSwitcherCloseBtn>
-          <ThemeSwitcher />
-          <CloseButton onClick={onClose} />
-          </ThemeSwitcherCloseBtn>
-          </LogoSwitcherStyled>
+            <LogoSwitcherStyled>
+                <Logo />
+                <ThemeSwitcherCloseBtn>
+                    <ThemeSwitcher />
+                    <CloseButton onClick={handleCloseModal}>Close</CloseButton>
+                </ThemeSwitcherCloseBtn>
+            </LogoSwitcherStyled>
             <NavigationLink />
         </NavigationContainer>
     );
