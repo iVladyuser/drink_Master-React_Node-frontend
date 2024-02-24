05ac001d4f46539f4baac5ae8e6e0c17e526839d
iVladyuser <korol.v09@gmail.com>
2024-02-24 16:50:33 +0100

    fixed SharedLayout

--- src/components/SharedLayout/SharedLayout.jsx
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -2,20 +2,19 @@ import { Suspense } from 'react';
 import { Outlet } from 'react-router-dom';
 import { Header } from '../Header/Header';
 import { Footer } from '../Footer/Footer';
-import { OutletDiv } from './SharedLayout.styled';
-// import PageEllipse222 from './Ellipse/Elipse222/Elipse222';
-// import PageEllipse224 from './Ellipse/Elipse224/Elipse224';
+import { OutletDiv, PngDiv } from './SharedLayout.styled';
+import PageEllipse222 from './Ellipse/Elipse222/Elipse222';
+import PageEllipse224 from './Ellipse/Elipse224/Elipse224';
 
 export const SharedLayout = () => {
   return (
     <>
-      {/* <PngDiv>
-        <PageEllipse222 />
-        <PageEllipse224 />
-      </PngDiv> */}
       <Suspense fallback={<div>Loading....</div>}>
         <Header />
-
+        <PngDiv>
+          <PageEllipse222 />
+          <PageEllipse224 />
+        </PngDiv>
         <OutletDiv>
           <Outlet />
         </OutletDiv>
