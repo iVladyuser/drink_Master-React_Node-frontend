cda55b169ff8421f839a964f3f06c92350af9c39
Mark <markiyan.chornovil@gmail.com>
2024-02-25 03:16:08 +0100

    update

--- src/services/fetchOwnDrinks.js
+++ src/services/fetchOwnDrinks.js
@@ -45,7 +45,7 @@ export const addOwnDrinkThunk = createAsyncThunk(
 
       const config = {
         headers: {
-          'Content-Type': 'multipart/form-data',
+          'Content-Type': 'application/json',
           Authorization: `Bearer ${token}`,
         },
       };
