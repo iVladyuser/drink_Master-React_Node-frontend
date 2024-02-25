bb7f0b0c03ff7acf58e03e2da1520acec2caea71
Vladislav-creator <130439019+Vladislav-creator@users.noreply.github.com>
2024-02-25 09:56:05 +0300

    55

---
+++ src/redux/drink/sliceForDrinksPages.js
@@ -0,0 +1,42 @@
+import { createSlice, isAnyOf, isRejectedWithValue } from '@reduxjs/toolkit';
+import { searchDrinks s } from '../../services/fetchDrinksForDrinksPages';
+const drinksInitialState = {
+  items: [],
+  isLoading: false,
+  error: null,
+};
+const extraActions = [searchDrinks ];
+const getActions = type =>
+  isAnyOf(
+    ...extraActions.map(action => {
+      return action[type];
+    })
+  );
+const fulfilledReducer = state => {
+  state.isLoading = false;
+  state.error = null;
+};
+const handlePending = state => {
+  state.isLoading = true;
+};
+
+const handleRejected = (state, action) => {
+  state.isLoading = false;
+  state.error = isRejectedWithValue(action.payload);
+};
+
+const drinksSlice = createSlice({
+  name: 'alldrinks',
+  initialState: drinksInitialState,
+  extraReducers: builder =>
+    builder
+      .addCase(getMainPageAllDrinks.fulfilled, (state, action) => {
+        state.items = action.payload;
+      })
+      .addMatcher(getActions('pending'), handlePending)
+      .addMatcher(getActions('fulfilled'), fulfilledReducer)
+      .addMatcher(getActions('rejected'), handleRejected),
+});
+export const allDrinksReduser = drinksSlice.reducer;
+
+
