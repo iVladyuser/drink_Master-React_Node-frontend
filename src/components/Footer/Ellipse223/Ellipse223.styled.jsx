4a30e3489255460920f96220ba7cd63fa6062e75
DimaRomanets <130175375+DimaRomanets@users.noreply.github.com>
2024-02-25 23:25:19 +0100

    styled

--- src/components/Footer/Ellipse223/Ellipse223.styled.jsx
+++ src/components/Footer/Ellipse223/Ellipse223.styled.jsx
@@ -11,23 +11,23 @@ export const Ellipse223 = styled.img`
     bottom: 1px;
     left: -100px;
 
-    animation-name: animateEllipse;
-    animation-duration: 3s;
-    animation-timing-function: linear;
-    animation-iteration-count: infinite;
-    animation-direction: alternate;
+    // animation-name: animateEllipse;
+    // animation-duration: 3s;
+    // animation-timing-function: linear;
+    // animation-iteration-count: infinite;
+    // animation-direction: alternate;
 
-    @keyframes animateEllipse {
-      0% {
-        opacity: 0;
-      }
-      50% {
-        opacity: 0.5;
-      }
-      100% {
-        opacity: 1;
-      }
-    }
+    // @keyframes animateEllipse {
+    //   0% {
+    //     opacity: 0;
+    //   }
+    //   50% {
+    //     opacity: 0.5;
+    //   }
+    //   100% {
+    //     opacity: 1;
+    //   }
+    // }
   }
 
   @media (min-width: 1440px) {
