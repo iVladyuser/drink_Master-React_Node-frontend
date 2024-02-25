3f4ec591d26e9aa1c5807fcdb1a735346fcbe8fc
iVladyuser <korol.v09@gmail.com>
2024-02-25 20:14:12 +0100

    delete selector

--- src/redux/selectors.js
+++
@@ -1,9 +0,0 @@
-export const selectIsLoading = state => state.auth.isLoading;
-// user selecctors
-
-export const selectEmail = state => state.auth.user.email;
-export const selectName = state => state.auth.user.name;
-export const selectIsRefreshing = state => state.auth.isRefreshing;
-
-export const selectUserBirthDate = state => state.auth.user.dateBirth;
-export const selectavatarURL = state => state.auth.user.avatarURL;
