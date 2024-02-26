cde5c540473a669f0156382266d8f23b481376c2
OksanaLukashyk <130361592+OksanaLukashyk@users.noreply.github.com>
2024-02-26 10:53:59 +0200

    Update GlobalStyle.jsx

--- src/styles/GlobalStyle.jsx
+++ src/styles/GlobalStyle.jsx
@@ -132,23 +132,3 @@ a {
     color: inherit;
   }  
 `;
-
-export const Container = styled.div`
-  width: 320px;
-  margin: 24px auto 0;
-  padding: 0px 20px 40px;
-
-  @media screen and (min-width: 768px) {
-    width: 768px;
-    margin: 40px auto 0;
-    padding: 0px 32px 44px;
-  }
-
-  @media screen and (min-width: 1440px) {
-    width: 1440px;
-    display: flex;
-    justify-content: space-between;
-    margin: 20px auto 0;
-    padding: 0px 112px 56px;
-  }
-`;
