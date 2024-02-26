09ec67c42a97450a1dcfb49d098be2d51e78a4df
OksanaLukashyk <130361592+OksanaLukashyk@users.noreply.github.com>
2024-02-26 11:15:12 +0200

    Update GlobalStyle.jsx

--- src/styles/GlobalStyle.jsx
+++ src/styles/GlobalStyle.jsx
@@ -1,4 +1,4 @@
-import styled, { createGlobalStyle } from 'styled-components';
+import { createGlobalStyle } from 'styled-components';
 import 'modern-normalize';
 
 export const GlobalStyle = createGlobalStyle`
